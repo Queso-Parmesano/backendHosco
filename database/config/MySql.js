a50290511315ae6932a587680bd5bce12bf19a32
QuesoParmesano <pecazon03@gmail.com>
2025-11-11 10:19:20 -0300

    con dialect desde env

--- database/config/MySql.js
+++ database/config/MySql.js
@@ -7,14 +7,16 @@ const passwordDB = process.env.passwordDB
 const nameDB = process.env.nameDB
 const userDB = process.env.userDB
 const hostDB = process.env.hostDB
+const dialectDb = process.env.dialectDb
 
 console.log()
 
 const sequelize = new Sequelize(nameDB, userDB, passwordDB, {
     host: hostDB,
-    dialect: 'mysql',
+    dialect: dialectDb,
     logging: false,
     timezone: '-03:00'
 })
+
 
 export default sequelize
